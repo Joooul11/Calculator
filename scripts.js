46a125093ee66a70a7445a8097753f8c2442d980
Joooul11 <pavel.sinyagovskiy@gmail.com>
2024-06-06 17:39:14 -0700

    edited JS

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function display(){
+
+}
+
+function numericButton(){
+
+}
+
+function divion (){}
+
+function multiplication(){}
+
+function subtraction(){}
+
+function equal (){}
+
+function addition (){}
+
+function decmial(){}
